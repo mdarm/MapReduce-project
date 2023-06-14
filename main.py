e391ceb8b95ffe7a29a55b11aaa3603cae95fc24
mdarm <michaledarm@gmail.com>
2023-06-15 00:24:32 +0300

    Added time averaging and uncertainty calculations.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-# main.py
+#*.txt main.py
 from rdd import query1 as rdd_query1
 from sql_parquet import query1 as sql_parquet_query1
 from sql_csv import query1 as sql_csv_query1
 # Import other queries...
 from csv_to_parquet import convert_csv_to_parquet
+
+# Import necessary functions
+from utils import timeit, calculate_average, calculate_error_margin
 
 def main():
     # Convert CSVs to Parquet
@@ -17,9 +20,16 @@ def main():
     times['sql_csv_query1'], _ = sql_csv_query1()
 
     # Write times to a text file
+    #with open('times.txt', 'w') as f:
+    #    for query, time in times.items():
+    #        f.write("%s: %f seconds\n" % ((query), (time)))
+    
+    # Compute averages and error margins and write to a text file
     with open('times.txt', 'w') as f:
-        for query, time in times.items():
-            f.write("%s: %f seconds\n" % ((query), (time)))
+        for query, time_list in times.items():
+            avg_time = calculate_average(time_list)
+            error_margin = calculate_error_margin(time_list, avg_time)
+            f.write('%s: %f with an uncertainty of %f seconds\n' % (query, avg_time, error_margin))
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,12 +1,55 @@
-# utils.py
 import time
 
 def timeit(func, *args, **kwargs):
-    """Measure execution time of a function"""
-    start = time.time()
-    result = func(*args, **kwargs)
-    end = time.time()
-    execution_time = end - start
-    print('Execution time: %f seconds' % (execution_time))
+    """
+    Measure execution time of a function for multiple runs.
+    
+    Args:
+        func: Function to be executed.
+        *args: Variable length argument list for the function.
+        **kwargs: Arbitrary keyword arguments for the function.
+    
+    Returns:
+        tuple: A tuple containing the list of execution times and the result of the last function call.
+    """
+    runs = 5  # change this to the number of runs you want
+    times = []
 
-    return execution_time, result
+    for i in range(runs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        times.append(end - start)
+
+    return times, result
+
+
+def calculate_average(times):
+    """
+    Calculate the average of a list of numbers.
+    
+    Args:
+        times: List of numbers to calculate the average of.
+    
+    Returns:
+        float: Average value.
+    """
+    total_time = sum(times)
+    return total_time / len(times)
+
+
+def calculate_error_margin(times, average):
+    """
+    Calculate the error margin for a list of numbers given the average.
+    
+    Args:
+        times: List of numbers to calculate the error margin for.
+        average: Average value.
+    
+    Returns:
+        float: Error margin.
+    """
+    squared_diffs = [(time - average) ** 2 for time in times]
+    variance = sum(squared_diffs) / len(times)
+    standard_deviation = variance ** 0.5
+    return 2 * standard_deviation
