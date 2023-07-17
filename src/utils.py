2af2ca7d92c3b4c3d6f3ddc22de38a39785cd16a
mdarm <michaledarm@gmail.com>
2023-07-18 00:18:45 +0300

    Removed statistical significance.

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import time
 
 def timeit(func, *args, **kwargs):
     """
-    Measure execution time of a function for multiple runs.
+    Measure execution time of a function for a single run.
     
     Args:
         func: Function to be executed.
@@ -10,47 +10,11 @@ def timeit(func, *args, **kwargs):
         **kwargs: Arbitrary keyword arguments for the function.
     
     Returns:
-        tuple: A tuple containing the list of execution times and the result of the last function call.
+        tuple: A tuple containing the execution time and the result of the function call.
     """
-    runs = 5 
-    times = []
+    start = time.time()
+    result = func(*args, **kwargs)
+    end = time.time()
+    execution_time = end - start
 
-    for i in range(runs):
-        start = time.time()
-        result = func(*args, **kwargs)
-        end = time.time()
-        times.append(end - start)
-
-    return times, result
-
-
-def calculate_average(times):
-    """
-    Calculate the average of a list of numbers.
-    
-    Args:
-        times: List of numbers to calculate the average of.
-    
-    Returns:
-        float: Average value.
-    """
-    total_time = sum(times)
-    return total_time / len(times)
-
-
-def calculate_error_margin(times, average):
-    """
-    Calculate the error margin for a list of numbers given the average.
-    
-    Args:
-        times: List of numbers to calculate the error margin for.
-        average: Average value.
-    
-    Returns:
-        float: Error margin with a confidence interval of two standard
-               deviations.
-    """
-    squared_diffs = [(time - average) ** 2 for time in times]
-    variance = sum(squared_diffs) / (len(times) - 1)
-    standard_deviation = variance ** 0.5
-    return 2 * standard_deviation
+    return execution_time, result
